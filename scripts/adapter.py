e0f5bdfc5c2557fc6fa33b152640279a660fcfa7
Moon Kwon Kim <mkdmkk@gmail.com>
2014-10-22 23:15:22 +0900

    draft

--- scripts/adapter.py
+++ scripts/adapter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 """
-concert_adapter/adapter.py v0.1
+adapter/adapter.py v0.1
 
 This module is to adapt requests from the BPEL engine to the concert framework.
 
@@ -12,15 +12,12 @@ Since: 2014.10.20
 from BaseHTTPServer import HTTPServer
 import sys
 import threading
-import time
 from pysimplesoap.server import SoapDispatcher, SOAPHandler
-import unique_id
 import yaml
 
 
 # Import Dependent Modules of ROS, Rocon, and Concert
 import rospy
-import rospkg
 import rocon_python_comms
 import rocon_uri
 import concert_service_utilities
@@ -28,26 +25,21 @@ import concert_scheduler_requests
 import concert_service_link_graph
 
 
-# Import Dependent Modules of Concert Adapter
-from server import AdapterSOAPServer
-
-
 # Import Messages
-import std_msgs.msg.String
-import concert_service_msgs.msg as concert_service_msgs
 import rocon_std_msgs.msg as rocon_std_msgs
 import scheduler_msgs.msg as scheduler_msgs
 
 
 # Constants
-NODE_NAME = 'concert_adapter'
+NODE_NAME = 'adapter'
 LOGGING_TAG = NODE_NAME + "/adapter"
+DEBUGING_TAG = "[DEBUG]" + LOGGING_TAG
 DEFAULT_QUEUE_SIZE = 1024
 SOAP_SERVER_HOST_NAME = 'localhost'
 SOAP_SERVER_PORT_NUMBER = '8008'
 
 
-class ConcertAdapter:
+class ConcertAdapter(object):
     __slots__ = [
         'soap_server',
         'service_name',
@@ -70,13 +62,13 @@ class ConcertAdapter:
             rospy.logerr("%s: Could not locate the scheduler's known_resources topic. [%s]" % (LOGGING_TAG, str(e)))
             sys.exit(1)
         # Setting up the requester
-        self.set_requester(self.service_id)
+        self._set_requester(self.service_id)
         # Starting the SOAP server
         self.start_soap_server
 
 
 #####################################################################
-# SOAP server related methods
+# Preparation for adaptation: SOAP Server
 #####################################################################
     def start_soap_server(self):
         """
@@ -94,7 +86,42 @@ class ConcertAdapter:
         dispatcher = SoapDispatcher('op_adapter_soap_disp', location = self.address, action = self.address,
                 namespace = "http://smartylab.co.kr/products/op/adapter", prefix="tns", trace = True, ns = True)
         dispatcher.register_function('adapt', self.on_service_invocation_received, returns={'out': str},
-                args={'nodeID': str, 'resourceName': str, 'duration': str, 'options': str})
+            args={
+                'LinkGraph': {
+                    'name': str,
+                    'nodes': [{
+                        'Node':{
+                            'id': str,
+                            'uri': str,
+                            'min': int,
+                            'max': int,
+                            'parameters': [{
+                                'parameter': {
+                                    'message': str,
+                                    'frequency': int
+                                }
+                            }]
+                        }
+                    }],
+                    'topics': [{
+                        'Topic':{
+                            'id': str,
+                            'type': str
+                        }
+                    }],
+                    'edges': [{
+                        'Edge':{
+                            'start': str,
+                            'finish': str,
+                            'remap_from': str,
+                            'remap_to': str
+                        }
+                    }]
+                }
+            }
+        )
+        # Single Node Registeration
+        #
         print("%s: Starting a SOAP server...", LOGGING_TAG)
         httpd = HTTPServer(("", int(SOAP_SERVER_PORT_NUMBER)), SOAPHandler)
         httpd.dispatcher = dispatcher
@@ -103,17 +130,10 @@ class ConcertAdapter:
         httpd.serve_forever()
 
 
-    def on_service_invocation_received(self, linkgraph):
-        # To validate the linkgraph
-        #
-        # To allocate resources
-        self.inquire_resources_to_allocate(linkgraph)
-
-
 #####################################################################
-# Resource allocation related methods
+# Preparation for adaptation: Requester
 #####################################################################
-    def set_requester(self, uuid):
+    def _set_requester(self, uuid):
         """
         To set a requester
         :param uuid:
@@ -121,13 +141,30 @@ class ConcertAdapter:
         """
         try:
             scheduler_requests_topic = concert_service_utilities.find_scheduler_requests_topic()
-            self.requester = concert_scheduler_requests.Requester(self.on_requester_reply_received, uuid=self.service_id, topic=scheduler_requests_topic)
+            self.requester = concert_scheduler_requests.Requester(self._on_requester_reply_received, uuid=self.service_id, topic=scheduler_requests_topic)
         except rocon_python_comms.NotFoundException as e:
             rospy.logerr("%s: Could not locate the scheduler's scheduler_requests topic. [%s]" % LOGGING_TAG, str(e))
             sys.exit(1)
 
 
-    def on_requester_reply_received(self, request_set):
+#####################################################################
+# Communication between the BPEL engine and the SOAP server
+#####################################################################
+    def _on_service_invocation_received(self, linkgraph):
+        # To validate the linkgraph
+        #
+        # To allocate resources
+        self._inquire_resources_to_allocate(linkgraph)
+
+
+    def _on_single_node_service_invocation_received(self, node):
+        pass
+
+
+#####################################################################
+# Resource allocation related methods
+#####################################################################
+    def _on_requester_reply_received(self, request_set):
         for request_id, request in request_set.requests.iteritems():
 
             if request.msg.status == scheduler_msgs.Request.GRANTED:
@@ -140,13 +177,15 @@ class ConcertAdapter:
                 self.granted_requests.remove(request_id)
 
 
-    def inquire_resources_to_allocate(self, linkgraph):
+    def _inquire_resources_to_allocate(self, linkgraph):
         """
         Let the requester allocate resources specified in the linkgraph
 
         :param linkgraph:
         :return:
         """
+
+        print("%s: linkgraph [%s]" % (DEBUGING_TAG, linkgraph))
 
         result = False
         print("%s: allocating resources..." % LOGGING_TAG)
@@ -161,11 +200,11 @@ class ConcertAdapter:
         self.requester.send_requests()
 
 
-    def on_resource_allocated(self, msg):
+    def _on_resource_allocated(self, msg):
         pass
 
 
-    def call_resource(self, resource, params):
+    def _call_resource(self, resource, params):
         """
 
         :param resource:
@@ -198,11 +237,53 @@ class ConcertAdapter:
 
 
 #####################################################################
-# Main method to launch the concert_adapter
+# Tester (will be removed)
+#####################################################################
+class Tester(threading.Thread):
+    def __init__(self, adapter):
+        chatter_linkgraph_yaml = yaml.load("""
+            name: "Chatter Concert"
+            nodes:
+              - id: dudes
+                uri: rocon:/*/*#rocon_apps/listener
+                min: 2
+                max: 4
+              - id: dudette
+                uri: rocon:/*/dudette#rocon_apps/talker
+                parameters:
+                  message: hello world
+                  frequency: 15
+            topics:
+              - id: chatter
+                type: std_msgs/String
+            actions: []
+            edges:
+              - start: chatter
+                finish: dudes
+                remap_from: chatter
+                remap_to: /conversation/chatter
+              - start: dudette
+                finish: chatter
+                remap_from: chatter
+                remap_to: /conversation/chatter
+        """)
+        impl_name, impl = concert_service_link_graph.load_linkgraph_from_yaml(chatter_linkgraph_yaml)
+        adapter._inquire_resources_to_allocate(impl)
+
+
+    def run(self):
+        pass
+
+
+#####################################################################
+# Main method to launch the adapter
 #####################################################################
 if __name__ == '__main__':
+    print("%s: Starting the concert adapter..." % LOGGING_TAG)
+    rospy.loginfo("%s: Starting the concert adapter..." % LOGGING_TAG)
     rospy.init_node(NODE_NAME)
     adapter = ConcertAdapter()
+    #tester = Tester(adapter) # to be removed
     rospy.spin()
     if not rospy.is_shutdown():
-        adapter.cancel_all_requests()
+        adapter.release_allocated_resources()
