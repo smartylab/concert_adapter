5371f84102f8bfad89df8156eed5abfa181deac7
Moon Kwon Kim <mkdmkk@gmail.com>
2014-12-16 12:23:14 +0900

    latest

--- scripts/tester.py
+++ scripts/tester.py
@@ -53,3 +53,44 @@ class ChatterTester(threading.Thread):
         rospy.loginfo("Allocating with the sample linkgraph...")
         self.adapter._inquire_resources_to_allocate(self.linkgraph)
 
+
+class TeleopTester(threading.Thread):
+    __slots__ = [
+        'linkgraph'
+    ]
+
+
+    def __init__(self, adapter):
+        threading.Thread.__init__(self)
+        self.adapter = adapter
+        linkgraph_yaml = yaml.load("""
+            name: "Teleop"
+            nodes:
+              - id: turtlebot
+                uri: rocon:/turtlebot
+                min: 1
+                max: 1
+            topics:
+              - id: teleop
+                type: geometry_msgs/Twist
+            actions: []
+            edges:
+              - start: teleop
+                finish: turtlebot
+                remap_from: teleop
+                remap_to: /conversation/teleop
+              - start: turtlebot
+                finish: teleop
+                remap_from: teleop
+                remap_to: /conversation/teleop
+        """)
+        impl_name, impl = concert_service_link_graph.load_linkgraph_from_yaml(linkgraph_yaml)
+        rospy.loginfo("Sample linkgraph loaded:\n%s" % impl)
+        self.linkgraph = impl
+
+
+    def run(self):
+        time.sleep(10)
+        rospy.loginfo("Allocating with the sample linkgraph...")
+        self.adapter._inquire_resources_to_allocate(self.linkgraph)
+
